2e5c328692c668679005e110160996f092b1bf9d
Rinaroar <marina.luxin@gmail.com>
2019-04-30 20:29:18 +0200

    Added JS file and continued the HTML/CSS

---
+++ landing.js
@@ -0,0 +1,17 @@
+
+
+let button = document.querySelectorAll('.external')
+
+for (let i=0; i<button.length; i++){
+    let jumbotron = button[i]
+
+    jumbotron.addEventListener('click', function(e){
+
+        let response = window.confirm('Etes vous certain de vouloir quitter le site ?')
+        if (response === false){
+            e.preventDefault()
+        }
+    })
+}
+
+
